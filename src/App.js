911f30b5577c831d28cd80528374dcaad9acb72c
kar-07-thik <karthi533110@gmail.com>
2025-12-27 10:57:34 +0530

    update project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import Navbar from "./components/Navbar.js"
+import SearchBox from "./components/SearchBox.js"
+import Product from "./components/Product.js"
+import About from "./components/About.js"
+import Footer from "./components/Footer.js"
+function App(){
+    return(<div>
+  <Navbar></Navbar>
+  <SearchBox></SearchBox>
+ <Product></Product>
+ <About></About>
+ <Footer></Footer>
+</div>
+
+    )
+}
+export default App
---
+++ src/components/SearchBox.js
@@ -0,0 +1,13 @@
+
+//sreach box component
+function SearchBox(){
+  return(
+     <div class="searchbar">
+         
+          
+            <input type="text" placeholder="Search"></input> 
+           
+        </div>
+  )
+}
+export default SearchBox
